d75612772af746acea8b8b3a65df7bb04c1618c3
ma-rtin <weiler.martin@gmx.net>
2018-12-28 17:52:11 +0100

    first source commit

---
+++ snake.py
@@ -0,0 +1,59 @@
+
+def printField(field):
+	
+
+	fieldString =''
+
+	for row in range(len(field)):
+		for col in range(len(field[0])):
+			fieldString += field[row][col]
+
+		fieldString += '\n'
+
+	print(fieldString)
+
+
+def initField(numRows, numCols):
+	field=[]
+
+	for rowIdx in range(numRows):
+		row = []
+		for colIdx in range(numCols):
+			if rowIdx == 0 or rowIdx==numRows-1 or colIdx == 0 or colIdx == numCols-1:
+				row.append('*')
+			else:
+				row.append(' ')
+
+		field.append(row)
+
+	return field
+
+def get_input(text):
+    return input(text)
+
+def readUserInput():
+	userInput = get_input('Enter your input:') 
+	if userInput == 'q':
+		#quit()
+		print('quit')
+		return 'quit'
+	elif userInput == 'p':
+		field = initField(1,40)
+		printField(field)
+	else:
+		#print(userInput)
+		print('continue')
+		return 'continue'
+
+
+if __name__ == '__main__':
+	while True:
+		readUserInput()
+
+
+
+def testFunktion():
+	print('ich bin eine testFunktion')
+
+def testQuadrat(zahl):
+	return zahl*zahl
